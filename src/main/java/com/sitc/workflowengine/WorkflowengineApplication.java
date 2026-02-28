5f7d6dae1a433972ba9a9163d33f3a92f292f091
ahmadhabbuddin <ahmad.habbuddin@gmail.com>
2026-02-28 20:42:29 +0700

    feat: initial project

---
+++ src/main/java/com/sitc/workflowengine/WorkflowengineApplication.java
@@ -0,0 +1,13 @@
+package com.sitc.workflowengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowengineApplication.class, args);
+	}
+
+}
